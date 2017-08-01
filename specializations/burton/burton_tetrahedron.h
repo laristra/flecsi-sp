f187eb63bef2f9a6e88bcdcd53c3358fdeaa6881
Navamita Ray <nray@lanl.gov>
2017-08-01 14:12:29 -0600

    Fixing year in copyright headers

--- specializations/burton/burton_tetrahedron.h
+++ specializations/burton/burton_tetrahedron.h
@@ -1,5 +1,5 @@
 /*~--------------------------------------------------------------------------~*
- * Copyright (c) 2017 Los Alamos National Laboratory, LLC
+ * Copyright (c) 2016-2017 Los Alamos National Laboratory, LLC
  * All rights reserved
  *~--------------------------------------------------------------------------~*/
 /*!
