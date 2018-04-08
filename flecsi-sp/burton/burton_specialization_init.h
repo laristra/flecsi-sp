93cfe1f36e4fdcba6fbda4a5bf64c371d3927df4
Marc Charest <charest@lanl.gov>
2018-04-07 19:26:41 -0600

    merged corners and wedges partitioning

--- flecsi-sp/burton/burton_specialization_init.h
+++ flecsi-sp/burton/burton_specialization_init.h
@@ -743,368 +743,11 @@ auto make_wedges( const MESH_DEFINITION & mesh_def )
 
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////
 /// \brief the main cell coloring driver
 ////////////////////////////////////////////////////////////////////////////////
 void partition_mesh( utils::char_array_t filename ) 
-{
-  // set some compile time constants 
-  constexpr auto num_dims = burton_mesh_t::num_dimensions;
-  constexpr auto cell_dim = num_dims;
-  constexpr auto thru_dim = 0;
-
-  // make some type aliases
-  using real_t = burton_mesh_t::real_t;
-  using exodus_definition_t = flecsi_sp::io::exodus_definition__<num_dims, real_t>;
-  using entity_info_t = flecsi::coloring::entity_info_t;
-  
-  // load the mesh
-  auto filename_string = filename.str();
-  exodus_definition_t mesh_def( filename_string );
-
-  // Create a communicator instance to get neighbor information.
-  auto communicator = std::make_unique<flecsi::coloring::mpi_communicator_t>();
-  auto comm_size = communicator->size();
-  auto rank = communicator->rank();
-
-
-  // create a vector of colorings and color info for each dimensional entity
-  std::vector< flecsi::coloring::index_coloring_t > 
-    entities( num_dims+1 );
-  std::vector< flecsi::coloring::coloring_info_t >
-    entity_color_info( num_dims+1 );
-
-  //----------------------------------------------------------------------------
-  // Cell Coloring
-  //----------------------------------------------------------------------------
-    
-  // Cells index coloring.
-  auto & cells = entities[ num_dims ];
-  auto & cell_color_info = entity_color_info[ num_dims ];
- 
-  // Create the dCRS representation for the distributed colorer.
-  // This essentialy makes the graph of the dual mesh.
-  auto dcrs = flecsi::coloring::make_dcrs(mesh_def);
-
-  // Create a colorer instance to generate the primary coloring.
-  auto colorer = std::make_unique<flecsi::coloring::parmetis_colorer_t>();
-
-  // Create the primary coloring.
-  cells.primary = colorer->color(dcrs);
-
-  //----------------------------------------------------------------------------
-  // Cell Closure.  However many layers of ghost cells are needed are found 
-  // here.
-  //----------------------------------------------------------------------------
-    
-  // Compute the dependency closure of the primary cell coloring
-  // through vertex intersections (specified by last argument "1").
-  // To specify edge or face intersections, use 2 (edges) or 3 (faces).
-  auto closure = 
-    flecsi::topology::entity_neighbors<cell_dim, cell_dim, thru_dim>(
-      mesh_def, cells.primary
-    );
-
-  // Subtracting out the initial set leaves just the nearest
-  // neighbors. This is similar to the image of the adjacency
-  // graph of the initial indices.
-  auto nearest_neighbors = 
-    flecsi::utils::set_difference(closure, cells.primary);
-
-  //----------------------------------------------------------------------------
-  // Find one more layer of ghost cells now, these are needed to get some corner
-  // cases right.
-  //----------------------------------------------------------------------------
-
-  // We can iteratively add halos of nearest neighbors, e.g.,
-  // here we add the next nearest neighbors. For most mesh types
-  // we actually need information about the ownership of these indices
-  // so that we can deterministically assign rank ownership to vertices.
-  auto nearest_neighbor_closure =
-    flecsi::topology::entity_neighbors<cell_dim, cell_dim, thru_dim>(
-      mesh_def, nearest_neighbors
-    );
-
-  // Subtracting out the closure leaves just the
-  // next nearest neighbors.
-  auto next_nearest_neighbors =
-    flecsi::utils::set_difference(nearest_neighbor_closure, closure);
-
-  // The union of the nearest and next-nearest neighbors gives us all
-  // of the cells that might reference a vertex that we need.
-  auto all_neighbors = 
-    flecsi::utils::set_union(nearest_neighbors, next_nearest_neighbors);
-
-  //----------------------------------------------------------------------------
-  // Find exclusive, shared, and ghost cells..
-  //----------------------------------------------------------------------------
-
-  // Get the rank and offset information for our nearest neighbor
-  // dependencies. This also gives information about the ranks
-  // that access our shared cells.
-  auto cell_nn_info =
-    communicator->get_primary_info(cells.primary, nearest_neighbors);
-
-  // Create a map version of the local info for lookups below.
-  std::unordered_map<size_t, size_t> primary_indices_map;
-  {
-    size_t offset(0);
-    for(auto i: cells.primary) 
-      primary_indices_map[offset++] = i;
-  } // scope
-
-  // Populate exclusive and shared cell information.
-  {
-    size_t offset(0);
-    for(auto i: std::get<0>(cell_nn_info)) {
-      // get the entity info
-      auto entry =
-        entity_info_t(primary_indices_map.at(offset), rank, offset, i);
-      // if it belongs to other colors, its a shared cell
-      if(i.size()) {
-        cells.shared.insert(entry);
-        // Collect all colors with whom we require communication
-        // to send shared information.
-        cell_color_info.shared_users = 
-          flecsi::utils::set_union(cell_color_info.shared_users, i);
-      }
-      // otherwise, its an exclusive cell
-      else 
-        cells.exclusive.insert(entry);
-      // increment counter
-      offset++;
-    } // for
-  } // scope
-  
-  // Populate ghost cell information.
-  for(auto i: std::get<1>(cell_nn_info)) {
-    cells.ghost.insert(i);
-    // Collect all colors with whom we require communication
-    // to receive ghost information.
-    cell_color_info.ghost_owners.insert(i.rank);
-  }
-  
-  // store the sizes of each set
-  cell_color_info.exclusive = cells.exclusive.size();
-  cell_color_info.shared = cells.shared.size();
-  cell_color_info.ghost = cells.ghost.size();
-
-  //----------------------------------------------------------------------------
-  // Create some maps for easy lookups when determining the other dependent
-  // closures.
-  //----------------------------------------------------------------------------
-
-  // Create a map version for lookups below.
-  std::unordered_map<size_t, entity_info_t> shared_cells_map;
-  for(auto i: cells.shared)
-    shared_cells_map[i.id] = i;
-
-  // Get the rank and offset information for all relevant neighbor
-  // dependencies. This information will be necessary for determining
-  // shared vertices.
-  auto cell_all_info = 
-    communicator->get_primary_info(cells.primary, all_neighbors);
-
-  // Create a map version of the remote info for lookups below.
-  std::unordered_map<size_t, entity_info_t> remote_info_map;
-  for(auto i: std::get<1>(cell_all_info)) 
-    remote_info_map[i.id] = i;
-
-  // Get the intersection of our nearest neighbors with the nearest
-  // neighbors of other ranks. This map of sets will only be populated
-  // with intersections that are non-empty
-  auto closure_intersection_map =
-    communicator->get_intersection_info(nearest_neighbors);
-
-  //----------------------------------------------------------------------------
-  // The Closures of other entities
-  //----------------------------------------------------------------------------
-  
-  for ( int i=0; i<num_dims; ++i ) {
-    color_entity( 
-      communicator.get(), 
-      closure, 
-      remote_info_map, 
-      shared_cells_map,
-      closure_intersection_map,
-      mesh_def.entities(num_dims, i),
-      mesh_def.entities(i, num_dims),
-      entities[i], 
-      entity_color_info[i]
-    );
-  }
-
-
-  //----------------------------------------------------------------------------
-  // Add the results to the context
-  //----------------------------------------------------------------------------
-
-  // Alias the index spaces type
-  using index_spaces = burton_mesh_t::index_spaces_t;
-
-  // Get the context instance.
-  auto & context = flecsi::execution::context_t::instance();
-
-  // Gather the coloring info from all colors
-  for ( int i=0; i<num_dims+1; ++i ) {
-    auto coloring_info = 
-      communicator->gather_coloring_info(entity_color_info[i]);
-    context.add_coloring( 
-      index_spaces::entity_map[0][i], entities[i], coloring_info
-    );
-  }
-    
-  //----------------------------------------------------------------------------
-  // add adjacency information
-  //----------------------------------------------------------------------------
-  
-  std::vector< std::vector<size_t> > entity_ids(num_dims+1);
-
-  // create a master list of all entities
-  for ( int i=0; i<num_dims+1; ++i ) {
-
-    const auto & these_entities = entities[i];
-    auto & these_ids = entity_ids[i];
-
-    // get the list of exclusive+shared+ghost ids
-    concatenate_ids( these_entities, these_ids );
-    auto num_entities = these_ids.size();
-
-    // sort the entities ( this is needed for the search down below )
-    // hoepefully it doesnt cause any problems
-    std::sort( these_ids.begin(), these_ids.end() );
-    auto last = std::unique( these_ids.begin(), these_ids.end() );
-    if ( last != these_ids.end() )
-      clog_error( "Duplicate ids in master lists" );
-
-  }
-
-  // loop over each dimension and determine the adjacency sizes
-  for ( int from_dim = 0; from_dim<=num_dims; ++from_dim ) {
-   
-    // the master list of all entity ids
-    const auto & from_ids = entity_ids[from_dim];
-
-    for ( int to_dim = 0; to_dim<=num_dims; ++to_dim ) {
-
-      // skip the case where both dimensions are the same
-      if ( from_dim == to_dim ) continue;
-
-      // the master list of all entity ids
-      const auto & to_ids = entity_ids[to_dim];
-      const auto to_ids_begin = to_ids.begin();
-      const auto to_ids_end = to_ids.end();
-
-      // populate the adjacency information
-      flecsi::coloring::adjacency_info_t ai;
-      ai.index_space = 
-        index_spaces::connectivity_map[ from_dim ][ to_dim ];
-      ai.from_index_space = index_spaces::entity_map[0][ from_dim ];
-      ai.to_index_space = index_spaces::entity_map[0][to_dim ];
-      ai.color_sizes.resize(comm_size);
-  
-      // loop over all cells and count the number of adjacencies
-      size_t cnt = 0;
-      for ( auto c : from_ids ) {
-        // get the attached sub entitites
-        const auto & ids = mesh_def.entities(from_dim, to_dim, c);
-        // we need to make sure they are in this colors master
-        // list though
-        for ( auto v : ids ) {
-          auto it = std::lower_bound( to_ids_begin, to_ids_end, v );
-          if ( it != to_ids_end && *it == v ) 
-            cnt++;
-        }
-      }
-  
-      // gather the results
-      ai.color_sizes = communicator->gather_sizes( cnt );
-    
-      // add the result to the context
-      context.add_adjacency(ai);
-
-    }
-  }
-
-
-  //----------------------------------------------------------------------------
-  // add intermediate mappings
-  //
-  // These are needed so that entities that are implicitly created by
-  // mesh.init<>() maintain consistent orderings that match the pre-computed
-  // colorings.
-  //----------------------------------------------------------------------------
-  
-  for ( int i=1; i<num_dims; ++i ) {
-
-    auto & entity_to_vertex_map =
-      context.intermediate_map( /* dim */ i, /* dom */ 0 ); 
-    auto & vertex_to_entity_map =
-      context.reverse_intermediate_map( /* dim */ i, /* dom */ 0 );
-
-    for ( auto e : entity_ids[i] ) { 
-      auto vs = mesh_def.entities( /* dimension */ i, /* domain */ 0, e );
-      // sorted for comparison later
-      std::sort( vs.begin(), vs.end() );
-      // add all the mappings for this entity
-      entity_to_vertex_map.emplace( e, vs ); 
-      vertex_to_entity_map.emplace( vs, e ); 
-    }
-
-  }
-
-  //----------------------------------------------------------------------------
-  // output the result
-  //----------------------------------------------------------------------------
-
-  // figure out this ranks file name
-  auto basename = ristra::utils::basename( filename_string );
-  auto output_prefix = ristra::utils::remove_extension( basename );
-  auto output_filename = output_prefix + "-partition_rank" +
-    ristra::utils::zero_padded(rank) + ".exo";
-
-  // a lamda function to convert sets of entitiy_info_t's to vectors
-  // of ids
-  auto to_vec = [](const auto & list_in)
-  {
-    std::vector<size_t> list_out;
-    list_out.reserve( list_in.size() );
-    for ( auto & e : list_in )
-      list_out.push_back( e.id );
-    return list_out;
-  };
-
-  // get a reference to the vertices
-  const auto & vertices = entities[0];
-
-  // open the exodus file
-  if ( rank == 0 )
-    std::cout << "Writing mesh to: " << output_filename << std::endl;
-
-  using std::make_pair;
-  mesh_def.write(
-    output_filename,
-    { 
-      make_pair( "exclusive cells", to_vec(cells.exclusive) ),
-      make_pair( "shared cells", to_vec(cells.shared) ),
-      make_pair( "ghost cells", to_vec(cells.ghost) )
-    },
-    { 
-      make_pair( "exclusive vertices", to_vec(vertices.exclusive) ),
-      make_pair( "shared vertices", to_vec(vertices.shared) ),
-      make_pair( "ghost vertices", to_vec(vertices.ghost) )
-    }
-  );
-
-  clog(info) << "Finished mesh partitioning." << std::endl;
-
-
-} // somerhing
-
-////////////////////////////////////////////////////////////////////////////////
-/// \brief the main cell coloring driver
-////////////////////////////////////////////////////////////////////////////////
-void partition_mesh_with_corners_n_wedges( utils::char_array_t filename ) 
 {
   // set some compile time constants 
   constexpr auto num_dims = burton_mesh_t::num_dimensions;
@@ -1303,6 +946,8 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
   //----------------------------------------------------------------------------
   // Identify exclusive, shared and ghost for corners and wedges
   //----------------------------------------------------------------------------
+
+#ifdef FLECSI_SP_BURTON_MESH_EXTRAS
   
   auto corners = make_corners( mesh_def );
   const auto & cells_to_corners = corners.first[num_dims];
@@ -1327,46 +972,6 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
     + corner_entities.shared.size()
     + corner_entities.ghost.size();
 
-  for ( auto cl : cells.exclusive ) {
-    for ( auto cn : cells_to_corners.at(cl.id) ) {
-      auto found = false;
-      for ( auto cn_excl : corner_entities.exclusive ) {
-        if ( cn_excl.id == cn ) {
-          found = true;
-          break;
-        }
-      }
-      assert( found );
-    }
-  }
-
-  for ( auto cl : cells.shared ) {
-    for ( auto cn : cells_to_corners.at(cl.id) ) {
-      auto found = false;
-      for ( auto cn_excl : corner_entities.shared ) {
-        if ( cn_excl.id == cn ) {
-          found = true;
-          break;
-        }
-      }
-      assert( found );
-    }
-  }
-
-  for ( auto cl : cells.ghost ) {
-    for ( auto cn : cells_to_corners.at(cl.id) ) {
-      auto found = false;
-      for ( auto cn_excl : corner_entities.ghost ) {
-        if ( cn_excl.id == cn ) {
-          found = true;
-          break;
-        }
-      }
-      assert( found );
-    }
-  }
-
-
   auto wedges = make_wedges( mesh_def );
   const auto & cells_to_wedges = wedges.first[num_dims];
   const auto & wedges_to_cells = wedges.second[num_dims];
@@ -1390,6 +995,8 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
     + wedge_entities.shared.size()
     + wedge_entities.ghost.size();
 
+#endif // FLECSI_SP_BURTON_MESH_EXTRAS
+
   //----------------------------------------------------------------------------
   // Add the results to the context
   //----------------------------------------------------------------------------
@@ -1402,9 +1009,9 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
 
   // Gather the coloring info from all colors
   for ( int dom=0; dom<num_domains; ++dom ) {
-      // skip empty slots
-      if ( entities.count(dom) == 0 || entity_color_info.count(dom) == 0 ) 
-        continue;
+    // skip empty slots
+    if ( entities.count(dom) == 0 || entity_color_info.count(dom) == 0 ) 
+      continue;
     for ( int dim=0; dim<num_dims+1; ++dim ) {
       // skip empty slots
       if ( entities.at(dom).count(dim) == 0 || 
@@ -1492,6 +1099,9 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
     }
   }
 
+#ifdef FLECSI_SP_BURTON_MESH_EXTRAS
+  
+  //--- CORNERS and WEDGES
 
   flecsi::coloring::adjacency_info_t ai;
   auto & adjacency_info = context.adjacency_info();
@@ -1652,6 +1262,8 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
   ai.color_sizes = gathered_wedges;
   context.add_adjacency(ai);
 
+#endif // FLECSI_SP_BURTON_MESH_EXTRAS
+
   //----------------------------------------------------------------------------
   // add intermediate mappings
   //
@@ -1676,6 +1288,8 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
       vertex_to_entity_map.emplace( vs, e ); 
     }
   }
+  
+#ifdef FLECSI_SP_BURTON_MESH_EXTRAS
 
   //--- CORNERS
 
@@ -1742,6 +1356,8 @@ void partition_mesh_with_corners_n_wedges( utils::char_array_t filename )
     wedges_to_entities.emplace( w, entity_list );
     entities_to_wedges.emplace( entity_list, w );
   }
+
+#endif // FLECSI_SP_BURTON_MESH_EXTRAS
 
   //----------------------------------------------------------------------------
   // output the result
@@ -1853,7 +1469,6 @@ void initialize_mesh(
 // Task Registration
 ///////////////////////////////////////////////////////////////////////////////
 flecsi_register_mpi_task(partition_mesh, flecsi_sp::burton);
-flecsi_register_mpi_task(partition_mesh_with_corners_n_wedges, flecsi_sp::burton);
 flecsi_register_task(initialize_mesh, flecsi_sp::burton, loc,
     single|flecsi::leaf);
 
