a3689536926487b624b39e2039f335a98a12e5ee
Marc Charest <charest@lanl.gov>
2020-01-29 15:27:41 -0700

    fixed some clang errors
    need a flecsi fix for this to all work

--- flecsi-sp/burton/burton_specialization_init.h
+++ flecsi-sp/burton/burton_specialization_init.h
@@ -2004,7 +2004,7 @@ void make_corners(
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // get the number of dimensions
-  constexpr auto num_dims = mesh_def.dimension();
+  constexpr auto num_dims = std::decay_t<decltype(mesh_def)>::dimension();
 
   // get the connectvitiy from the cells to the vertices.  there is a
   // corner per cell, per vertex
@@ -2702,7 +2702,7 @@ void make_corners(
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // get the number of dimensions
-  constexpr auto num_dims = mesh_def.dimension();
+  constexpr auto num_dims = std::decay_t<decltype(mesh_def)>::dimension();
 
   // get the connectvitiy from the cells to the vertices.  there is a
   // corner per cell, per vertex
